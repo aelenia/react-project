eb348373026b90cc7496152ac22ce64a253d0af3
Fabian Pasin <fabian.pasin@gmail.com>
2018-05-16 15:46:17 +0200

    Add UserItem and UserEdit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
+import Form from './components/UserEdit'
+import UserItem from './components/UserItem'
+
+const users = [{ name: 'Peter', skills: { html: 5, css: 5, js: 7 } }]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <UserItem name={'Fabian'} skills={{ html: 9, css: 8, js: 7 }} />
+        <Form
+          name={'Fabian'}
+          skills={{ html: 9, css: 8, js: 7 }}
+          onEnter={event => console.log(event)}
+          onSave={event => console.log(event)}
+        />
       </div>
     )
   }
